cc7d8e95544517c0a0e4441d3c9d16fbb71122c3
Prashantraniga <prashant.raniga2012@gmail.com>
2018-10-04 13:54:13 +0100

    adding all previous code

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,53 @@
+
+require 'bike'
+
+describe DockingStation do
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  it "Returns true from working? method" do
+    s1 = DockingStation.new
+
+    expect(s1.release_bike.working?).to eq(true)
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.capacity.times { subject.dock Bike.new}
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+  describe 'initialization' do
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+      expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+    end
+  end
+
+  it "call the dock method for a bike released" do
+    bike = Bike.new
+    expect(DockingStation.new.dock(bike)).to eq "Bike Docked"
+  end
+
+end
